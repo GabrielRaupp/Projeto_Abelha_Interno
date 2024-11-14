afc29f562e965a02b6413272e8b1f2851fc8ab5e
gabriel raupp <gabiraupppimenteL@gmail.com>
2024-11-14 14:04:40 -0300

    versão corrijida3

--- src/frontend/Script.js
+++ src/frontend/Script.js
@@ -99,12 +99,6 @@ function drawLineChart(containerId, title, seriesData, unit) {
         tooltip.style.display = 'none';
     });
 
-    // Remove o botão de download anterior, se existir
-    const existingButton = chartElement.parentNode.querySelector('.download-button');
-    if (existingButton) {
-        existingButton.remove();
-    }
-
     // Adiciona o botão de download
     const downloadButton = document.createElement('button');
     downloadButton.innerText = 'Baixar Dados';
@@ -141,19 +135,8 @@ async function fetchData(selectedDate = today) {
         const data = await response.json();
         if (!Array.isArray(data) || data.length === 0) throw new Error("Dados no formato incorreto ou array vazio.");
 
-        // Ajustando o horário para o local, usando UTC para garantir que pegamos o dia correto
-        const startOfDay = new Date(Date.UTC(
-            new Date(selectedDate).getFullYear(),
-            new Date(selectedDate).getMonth(),
-            new Date(selectedDate).getDate(),
-            0, 0, 0
-        ));
-        const endOfDay = new Date(Date.UTC(
-            new Date(selectedDate).getFullYear(),
-            new Date(selectedDate).getMonth(),
-            new Date(selectedDate).getDate(),
-            23, 59, 59
-        ));
+        const startOfDay = new Date(`${selectedDate}T00:00:00Z`);
+        const endOfDay = new Date(`${selectedDate}T23:59:59Z`);
 
         const filteredData = data.filter(item => {
             const date = new Date(item.data);
@@ -208,7 +191,7 @@ dateSelector.addEventListener('change', selectDate);
 dateSelectorContainer.appendChild(dateSelector);
 document.body.prepend(dateSelectorContainer);
 
-// Atualiza os gráficos a cada 10 minutos
+// Atualiza os gráficos a cada 3 minutos
 setInterval(() => fetchData(), 600000);
 
 // Carrega os dados inicialmente quando a página é carregada
